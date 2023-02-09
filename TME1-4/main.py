77355198892b515b6fd65f972ff5d823ec174411
Paul Jarski <pjarski@sfcm.edu>
2023-02-09 20:46:22 +0100

    exo 2 terminé

--- TME1-4/main.py
+++ TME1-4/main.py
@@ -37,12 +37,12 @@ def lirePreferencesSpeSurEtu(nomFichier: str) -> tuple[list[list[int]],list[int]
 	
 
 def GaleShapleyCoteEtu(lPrefEtu: list[list[int]], capacites: list[int], lPrefSpe: list[list[int]]) -> list[tuple[int,int]]:
-	'''
+	"""
 	en entrée :	la matrice lPrefEtu[i,j] des préférences j pour chaque étudiant i
 				la liste d'entiers des capacités de chaque parcours
 				la matrice lPrefSpe[i,j] des préférences j pour chaque parcours i
 	en sortie :	un mariage parfait étudiant optimal M contenant des couples (étudiant, parcours)
-	'''
+	"""
 	#initialisation
 	prochaineProposition = [0 for i in range(len(lPrefEtu))]#indices des prochaines specialites auxquelles l'étudiant i n'a pas encore fait de proposition
 	etusLibres = [i for i in range(len(lPrefEtu))]			#étudiants pas encore affectés (chacun d'un unique indice i dans [0,n-1])
@@ -67,6 +67,7 @@ def GaleShapleyCoteEtu(lPrefEtu: list[list[int]], capacites: list[int], lPrefSpe
 					break
 			if not permutation:			
 				etusLibres.append(e)	#il faut remettre l'étudiant rejeté dans la liste
+
 		prochaineProposition[e] += 1
 
 	return M
@@ -74,12 +75,12 @@ def GaleShapleyCoteEtu(lPrefEtu: list[list[int]], capacites: list[int], lPrefSpe
 
 
 def GaleShapleyCoteSpe(lPrefEtu: list[list[int]], capacites: list[int], lPrefSpe: list[list[int]]) -> list[tuple[int,int]]:	
-	'''
+	"""
 	en entrée :	la matrice lPrefEtu[i,j] des préférences j pour chaque étudiant i
 				la liste d'entiers des capacités de chaque parcours
 				la matrice lPrefSpe[i,j] des préférences j pour chaque parcours i
 	en sortie :	un mariage parfait parcours optimal M contenant des tuples (étudiant, parcours)   ...même ordre dans les tuples pour faciliter la comparaison des mariages plus tard
-	'''
+	"""
 	#initialisation
 	prochaineProposition = [0 for i in range(len(lPrefSpe))]#indices des prochains étudiants auxquels le parcours i n'a pas encore fait de proposition
 	etusLibres = [i for i in range(len(lPrefEtu))]			#étudiants pas encore affectés (chacun d'un unique indice i dans [0,n-1])
@@ -105,12 +106,12 @@ def GaleShapleyCoteSpe(lPrefEtu: list[list[int]], capacites: list[int], lPrefSpe
 	return M
 
 def pairesInstables(M: list[tuple[int,int]], lPrefEtu: list[list[int]], lPrefSpe: list[list[int]]) -> list[tuple[int,int]]:
-	'''
+	"""
 	en entrée :	le mariage M représenté par des couples (étudiant, parcours)
 				la matrice lPrefEtu[i,j] des préférences j pour chaque étudiant i
 				la matrice lPrefSpe[i,j] des préférences j pour chaque parcours i
 	en sortie :	une liste de paires instables représentée par des couples (étudiant, parcours)
-	'''
+	"""
 
 	'''
 	#version sans compréhension des listes
@@ -125,72 +126,97 @@ def pairesInstables(M: list[tuple[int,int]], lPrefEtu: list[list[int]], lPrefSpe
 	return [(e1,p2) for e1,p1 in M for e2,p2 in M if lPrefEtu[e1].index(p2) < lPrefEtu[e1].index(p1) and lPrefSpe[p2].index(e1) < lPrefSpe[p2].index(e2)]
 
 
+
+def genererPrefEtu(n):	
+	lPrefEtu=[]
+	for i in range(n):
+		prefEtu = [i for i in range(9)] #9 parcours
+		random.shuffle(prefEtu)
+		lPrefEtu.append(prefEtu)
+	return lPrefEtu
+
+def genererPrefSpe(n):	
+	lPrefSpe=[]
+	for i in range(9):
+		prefSpe = [i for i in range(n)]
+		random.shuffle(prefSpe)
+		lPrefSpe.append(prefSpe)
+	return lPrefSpe
+
+def genererCap(n):
+	#la somme des capacites est de n du faite que le reste est rajoyte a la fins
+	cap = [n//9 for i in range(9)]
+	reste = n % 9 
+	for i in range(reste):
+		cap[i] += 1
+	return cap
+
+
 def genererPref(n: int) -> tuple[list[list[int]], list[int], list[list[int]]]:
-	'''
+	"""
 	en entrée:	le nombre d'étudiants n
 				(le nombre de parcours est constante: 9)
 	en sortie:	
 				la matrice lPrefEtu[i,j] des préférences j pour chaque étudiant i
 				la liste d'entiers des capacités de chacun des 9 parcours, générés de façon détérministe et plus ou moins égaux entre les parcours
 				la matrice lPrefSpe[i,j] des préférences j pour chaque parcours i
-	'''
-	
-	lPrefEtu=[]
-	for i in range(n):
-		prefEtu = [i for i in range(9)]
-		random.shuffle(prefEtu)
-		lPrefEtu.append(prefEtu)
-
-	lPrefSpe=[]
-	for i in range(9):
-		prefSpe = [i for i in range(n)]
-		random.shuffle(prefSpe)
-		lPrefSpe.append(prefSpe)
-	#la somme des capacites est de n du faite que le reste est rajoyte a la fins
-	cap = [n//9 for i in range(9)]
-	reste = n % 9 
-	for i in range(reste):
-		cap[i] += 1
-	return lPrefEtu, cap, lPrefSpe
+	"""
+	return genererPrefEtu(n), genererCap(n), genererPrefSpe(n)
 
 
 
-def temps_execution_gs_cote_etu(binf, bsup , step) :
+def temps_execution_gs_cote_etu(r:range, nb_tests:int) -> list[float]:
+	assert(nb_tests > 0)
 	temps = []
-	for i in range(binf , bsup, step):
-		
-		lpe,c,lps = genererPref(i)
-		tps1 = time.process_time()
-		GaleShapleyCoteEtu(lpe,c,lps)
-		tps2 = time.process_time()
-		temps.append(tps2-tps1)
+	for i in r:
+		total=0
+		for j in range(nb_tests):
+			lpe,c,lps = genererPref(i)
+			tps1 = time.process_time()
+			GaleShapleyCoteEtu(lpe,c,lps)
+			tps2 = time.process_time()
+			total += (tps2 - tps1)
+		temps.append(total/nb_tests)
 	return temps
 
-def temps_execution_gs_cote_spe(binf, bsup , step) :
+def temps_execution_gs_cote_spe(r:range, nb_tests:int) -> list[float]:
+	assert(nb_tests > 0)
 	temps = []
-	for i in range(binf , bsup, step):
-		
-		lpe,c,lps = genererPref(i)
-		tps1 = time.process_time()
-		GaleShapleyCoteSpe(lpe,c,lps)
-		tps2 = time.process_time()
-		temps.append(tps2-tps1)
+	for i in r:
+		total=0
+		for j in range(nb_tests):
+			lpe,c,lps = genererPref(i)
+			tps1 = time.process_time()
+			GaleShapleyCoteSpe(lpe,c,lps)
+			tps2 = time.process_time()
+			total += (tps2-tps1)
+		temps.append(total/nb_tests)
 	return temps
 
-def nb_iter_gs_cote_etu(r):
+def nb_iter_gs_cote_etu(r:range, nb_tests:int) -> list[float]:
+	assert(nb_tests > 0)
 	nb_iter = []
 	for i in r:
-		#GaleShapleyCoteEtu_nbIter(genererPref[i])[1] represente it retourné par la fonction GS
-		M,it = GaleShapleyCoteEtu_nbIter(genererPref(i))
-		nb_iter.append(it)
+		total=0
+		for j in range(nb_tests):
+			#GaleShapleyCoteEtu_nbIter(genererPref[i])[1] represente it retourné par la fonction GS
+			#M,it = GaleShapleyCoteEtu_nbIter(genererPref(i))
+			lPrefEtu, cap, lPrefSpe = genererPref(i)
+			total += GaleShapleyCoteEtu_nbIter(lPrefEtu, cap, lPrefSpe)[1]
+		nb_iter.append(total/nb_tests)
 	return nb_iter
 
-def nb_iter_gs_cote_spe(r):
+def nb_iter_gs_cote_spe(r:range, nb_tests:int) -> list[float]:
+	assert(nb_tests > 0)
 	nb_iter = []
 	for i in r:
-		#GaleShapleyCoteEtu_nbIter(genererPref[i])[1] represente it retourné par la fonction GS
-		M,it = GaleShapleyCoteSpe_nbIter(genererPref(i))
-		nb_iter.append(it)
+		total=0
+		for j in range(nb_tests):
+			#GaleShapleyCoteEtu_nbIter(genererPref[i])[1] represente it retourné par la fonction GS
+			#M,it = GaleShapleyCoteSpe_nbIter(genererPref(i))
+			lPrefEtu, cap, lPrefSpe = genererPref(i)
+			total += GaleShapleyCoteSpe_nbIter(lPrefEtu, cap, lPrefSpe)[1]
+		nb_iter.append(total/nb_tests)
 	return nb_iter
 
 
@@ -280,8 +306,6 @@ def tracer2courbes(x: list[int], y1: list[float], y1Label:str, y2: list[float], 
 	plt.show()
 
 
-	
-
 '''
 print("bonjour")
 maListe=exemple.lectureFichier("test.txt") # Execution de la methode lectureFichier du fichier exemple.
@@ -294,15 +318,16 @@ def main():
 	etuSurSpe = lirePreferencesEtuSurSpe("PrefEtu.txt")
 	cap, speSurEtu = lirePreferencesSpeSurEtu("PrefSpe.txt")	#couple (liste des capacités, matrice des préférences)
 
-	print('etuSurSpe:')
-	print(etuSurSpe)
-	print()
-	print('cap')
-	print(cap)
-	print()
-	print('speSurEtu:')
-	print(speSurEtu)
-	print()
+	###TESTS
+	#print('etuSurSpe:')
+	#print(etuSurSpe)
+	#print()
+	#print('cap')
+	#print(cap)
+	#print()
+	#print('speSurEtu:')
+	#print(speSurEtu)
+	#print()
 	assert(len(cap) == len(speSurEtu))	
 
 #Q2
@@ -329,26 +354,64 @@ def main():
 	print()
 
 #Q5
-	#cf genererPref qui fait le tout
+	#cf genererPref qui appelle les fonctions
 
 #Q6
-	#lpe,c,lps = genererPref(107)
-	#print(lpe)
-	#print(c)
-	#print(lps)
-	abscisse = range(200,3000,100)
-	#temps_cote_etu = temps_execution_gs_cote_etu(200,3000,100)
-	#temps_cote_spe = temps_execution_gs_cote_spe(200,3000,100)
-	#tracer2courbes(abscisse, temps_cote_etu, "côté étudiant",temps_cote_spe, "côté spécialisation", "temps (s)")
-#Q6
+
+#COMPLEXITE TEMPORELLE - GRAPHES
+
+	#on crée deux graphes:
+	fig, axes = plt.subplots(nrows=2, ncols=1) 
+
+
+	#Graphe des deux courbes ensemble:
+	abscisse = range(200,2000,200)
+	temps_cote_etu = temps_execution_gs_cote_etu(abscisse,nb_tests=10)
+	temps_cote_spe = temps_execution_gs_cote_spe(abscisse,nb_tests=10)
+	
+	axes[0].plot(abscisse, temps_cote_etu, color = "blue", label="côté étudiant")
+	axes[0].plot(abscisse, temps_cote_spe, color = "green", label="côté spécialité")
+	axes[0].legend()
+	axes[0].set_title("Temps d'exécution de GS coté étudiant, coté spécialité")
+	axes[0].set_xlabel("n")
+	axes[0].set_ylabel("temps (s)")
+
+	#Graphe de la courbe côté étudiant pour voir plus clairement l'évolution de son taux de croissance
+	abscisse = range(500,5000,500)
+	temps_cote_etu = temps_execution_gs_cote_etu(abscisse, nb_tests=10)
+
+	axes[1].plot(abscisse, temps_cote_etu, color = "blue", label="côté étudiant")
+	axes[1].legend()
+	axes[1].set_title("Temps d'exécution de GS côté étudiant")
+	axes[1].set_xlabel("n")
+	axes[1].set_ylabel("temps (s)")	
+
+	plt.tight_layout()
+	#plt.show()
+
 
 #Q7
+	#Oui, la complexité est polynomiale de degré strictement supérieur à 1.  C'est cohérent avec la complexité théorique de O(n**2)
 
 #Q8
 
-	it_cote_etu = nb_iter_gs_cote_etu(abscisse)
-	it_cote_spe = nb_iter_gs_cote_spe(abscisse)
-	tracer2courbes(list(abscisse), it_cote_etu, "côté étudiant", it_cote_spe, "côté spécialisation", "nombre d'itérations")
+	#NOMBRE MOYEN D'ITERATIONS - GRAPHES
+
+	#Un seul graphe suffit
+	fig2,axes2 = plt.subplots()
+
+	abscisse = range(200,2000,200)
+	it_cote_etu = nb_iter_gs_cote_etu(abscisse, 10)
+	it_cote_spe = nb_iter_gs_cote_spe(abscisse, 10)
+	axes2.plot(abscisse, it_cote_etu, color='blue', label='nb itérations côté étudiant')
+	axes2.plot(abscisse, it_cote_spe, color='green', label='nb itérations côté spécialité')
+	axes2.legend()
+	axes2.set_title("Nombre d'itérations de GS côté étudiant, côté spécialité")
+	axes2.set_xlabel("n")
+	axes2.set_ylabel("nombre d'itérations")
+	plt.show()
+
+	#On voit que le nombre d'itérations est linéaire par rapport à n.  C'est cohérent avec la complexité théorique, i.e. O(n) tours de boucles, chacun en O(n) pour une complexité globale de O(n**2)
 
 if __name__ == "__main__":
 	main()
